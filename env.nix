d6230890d3aa926f2f56ae2ebb9f749f5e407bfa
chengzh2008 <chengzh2008@gmail.com>
2022-12-23 17:22:09 -0800

    half way of migration from brew to nix

--- env.nix
+++ env.nix
@@ -1,10 +1,39 @@
 let pkgs = import <nixpkgs> {}; 
 in [
   pkgs.asciidoc
+  pkgs.automake
+  pkgs.autoconf
+  pkgs.azure-cli
+  # pkgs.awk
   pkgs.calc
+  pkgs.curl
+  pkgs.ctags
   pkgs.direnv
+  pkgs.docbook5
+  pkgs.expat
+  pkgs.fd
+  # pkgs.helm
+  pkgs.gd
+  pkgs.git
+  pkgs.graphviz
+  pkgs.grpc
+  pkgs.grpcurl
+  pkgs.jansson
   pkgs.jq
+  pkgs.lua
+  pkgs.maven
+  pkgs.nginx
+  # pkgs.openapi-generator
+  pkgs.pv
+  pkgs.restic
   pkgs.ripgrep
+  pkgs.sourceHighlight
+  pkgs.texinfo
   pkgs.tmux
   pkgs.tree
+  pkgs.tree-sitter
+  pkgs.utf8proc
+  pkgs.wget
+  pkgs.yarn
+  pkgs.zlib
 ]
