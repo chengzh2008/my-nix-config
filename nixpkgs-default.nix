7bc10062885fe55d5c411069c8e2bf8fb46b7671
chengzh2008 <chengzh2008@gmail.com>
2023-12-16 14:43:31 -0800

    stop using nix-config; and switchto use home-manager with flake; see directory ~/.config/home-manager

--- nixpkgs-default.nix
+++ nixpkgs-default.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/b7589ceaeea275918c209db1c9a2c51e327af1ee.tar.gz")
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a46b965ea7d1b9587a46f91adfdbac29e56c9b87.tar.gz")
